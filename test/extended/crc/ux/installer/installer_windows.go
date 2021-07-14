c70c29560b1de2d7d425224d52fcb01b100d2776
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-07-14 14:32:43 +0200

    e2e windows installer

--- test/extended/crc/ux/installer/installer_windows.go
+++ test/extended/crc/ux/installer/installer_windows.go
@@ -3,19 +3,46 @@
 package installer
 
 import (
+	"exec"
 	"fmt"
+	"time"
+
+	win32waf "github.com/adrianriobo/gowinx/pkg/win32/api/user-interface/windows-accesibility-features"
+	"github.com/adrianriobo/gowinx/pkg/win32/ux"
+	"github.com/code-ready/crc/pkg/crc/logging"
 )
 
-type handler struct {
-	currentUserPassword *string
-	installerPath       *string
+const (
+	installerWindowTitle string        = "CodeReady Containers Setup"
+	elementClickDelay    time.Duration = 2 * time.Second
+	installationTime     time.Duration = 30 * time.Second
+)
+
+type installerElement struct {
+	name   string
+	id     string
+	screen string
 }
 
-func NewInstaller(currentUserPassword *string, installerPath *string) Installer {
+var (
+	welcomeNextButton         = installerElement{name: "Next", id: "1", screen: "welcome"}
+	licenseAcceptCheck        = installerElement{name: "accept", id: "1", screen: "license"}
+	licenseNextButton         = installerElement{name: "Next", id: "3", screen: "license"}
+	destinantionNextButton    = installerElement{name: "Next", id: "1", screen: "destination"}
+	installationInstallButton = installerElement{name: "Install", id: "1", screen: "installation"}
+	finalizationFinishButton  = installerElement{name: "Finish", id: "1", screen: "finalization"}
+)
+
+type gowinxHandler struct {
+	CurrentUserPassword *string
+	InstallerPath       *string
+}
+
+func NewInstaller(currentUserPassword, installerPath *string) Installer {
 	// TODO check parameters as they are mandatory otherwise exit
-	return handler{
-		currentUserPassword: currentUserPassword,
-		installerPath:       installerPath}
+	return gowinxHandler{
+		CurrentUserPassword: currentUserPassword,
+		InstallerPath:       installerPath}
 
 }
 
@@ -23,6 +50,65 @@ func RequiredResourcesPath() (string, error) {
 	return "", nil
 }
 
-func (h handler) Install() error {
-	return fmt.Errorf("not implemented yet")
+func (g gowinxHandler) Install() error {
+	// Initialize context
+	win32waf.Initalize()
+	if err := runInstaller(*g.InstallerPath); err != nil {
+		return err
+	}
+	// Welcome screen
+	if err := clickButton(welcomeNextButton); err != nil {
+		return err
+	}
+	// License screen
+	if err := clickButton(licenseAcceptCheck); err != nil {
+		return err
+	}
+	if err := clickButton(licenseNextButton); err != nil {
+		return err
+	}
+	// Destination
+	if err := clickButton(destinantionNextButton); err != nil {
+		return err
+	}
+	// Installation
+	if err := clickButton(installationInstallButton); err != nil {
+		return err
+	}
+	// wait installation process
+	time.Sleep(installationTime)
+	// Finalization
+	if err := clickButton(finalizationFinishButton); err != nil {
+		return err
+	}
+	// TODO which should be executed from a new cmd (to ensure ENVs)
+	// Finalize context
+	win32waf.Finalize()
+	return nil
+}
+
+func runInstaller(installerPath string) error {
+	command := fmt.Sprintf("msiexec.exe /i %s /qf", installerPath)
+	if _, err := exec.Command(command); err != nil {
+		return fmt.Errorf("error starting the msi installer")
+	}
+	return nil
+}
+
+func clickButton(element installerElement) error {
+	// Ensure the installer is the active window
+	// Get action center window
+	if installerWindow, err := ux.GetActiveElement(installerWindowTitle, ux.WINDOW); err != nil {
+		return err
+	} else {
+		button, err := installerWindow.GetElement(element.name, ux.BUTTON)
+		if err != nil {
+			logging.Error(err)
+			return err
+		}
+		if err := button.Click(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
